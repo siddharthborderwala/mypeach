3af78327f02af50dd7575e3345caba914fdb0d54
Siddharth Borderwala <siddharthborderwala@gmail.com>
2024-10-03 23:13:30 +0530

    add watermark to thumbnail

--- src/trigger/generate-thumbnail.ts
+++ src/trigger/generate-thumbnail.ts
@@ -40,17 +40,51 @@ async function getInputStreamFromS3(
 async function createSharpTransform(
 	width: number,
 	quality: number,
+	addWatermark: boolean,
 ): Promise<sharp.Sharp> {
-	return sharp()
+	let transform = sharp()
 		.resize({
 			width: width,
 			fit: "inside",
 			withoutEnlargement: true,
 		})
-		.webp({ quality, effort: 6 })
-		.on("error", (error) => {
-			logger.error("Error in sharp transform:", { error });
-		});
+		.webp({ quality, effort: 6 });
+
+	if (addWatermark) {
+		// Load the watermark from https://usercontent.mypeach.in/watermark.png
+		const watermarkBuffer = await fetch(
+			"https://usercontent.mypeach.in/watermark.png",
+		).then((res) => res.arrayBuffer());
+
+		// Create a new sharp instance for the watermark
+		const watermarkWithMargin = await sharp(Buffer.from(watermarkBuffer))
+			.ensureAlpha()
+			.extend({
+				top: 60,
+				bottom: 60,
+				left: 80,
+				right: 80,
+				background: { r: 0, g: 0, b: 0, alpha: 0 }, // Transparent background
+			})
+			.rotate(-45, {
+				background: { r: 0, g: 0, b: 0, alpha: 0 },
+			})
+			.toBuffer();
+
+		// Add the watermark with margins to the image
+		transform = transform.composite([
+			{
+				input: watermarkWithMargin,
+				tile: true,
+				gravity: "northeast",
+			},
+		]);
+	}
+
+	return transform.on("error", (error: unknown) => {
+		logger.error("Error in sharp transform:", { error });
+		throw error;
+	});
 }
 
 async function uploadToPublicBucket(
@@ -73,13 +107,14 @@ async function uploadToPublicBucket(
 	await upload.done();
 }
 
-async function convertTiffToWebp({
+async function convertToWebp({
 	originalFileStorageKey,
 	thumbnailStorageKey,
 	width,
 	quality,
 	sourceBucket,
 	destinationBucket,
+	addWatermark,
 }: {
 	originalFileStorageKey: string;
 	thumbnailStorageKey: string;
@@ -87,13 +122,14 @@ async function convertTiffToWebp({
 	quality: number;
 	sourceBucket: string;
 	destinationBucket: string;
+	addWatermark: boolean;
 }): Promise<void> {
 	try {
 		const readableStream = await getInputStreamFromS3(
 			originalFileStorageKey,
 			sourceBucket,
 		);
-		const transform = await createSharpTransform(width, quality);
+		const transform = await createSharpTransform(width, quality, addWatermark);
 		await uploadToPublicBucket(
 			readableStream,
 			transform,
@@ -144,23 +180,25 @@ export const generateThumbnailTask = task({
 
 		try {
 			// Generate 2000w WebP
-			await convertTiffToWebp({
+			await convertToWebp({
 				originalFileStorageKey: payload.originalFileStorageKey,
 				thumbnailStorageKey: thumbnailStorageKey[2000],
 				width: 2000,
 				quality: 50,
 				sourceBucket: process.env.R2_PROTECTED_BUCKET_NAME!,
 				destinationBucket: process.env.R2_PUBLIC_BUCKET_NAME!,
+				addWatermark: true,
 			});
 
 			// generate 1200w WebP from 2000w WebP
-			await convertTiffToWebp({
+			await convertToWebp({
 				originalFileStorageKey: thumbnailStorageKey[2000],
 				thumbnailStorageKey: thumbnailStorageKey[1200],
 				width: 1200,
 				quality: 50,
 				sourceBucket: process.env.R2_PUBLIC_BUCKET_NAME!,
 				destinationBucket: process.env.R2_PUBLIC_BUCKET_NAME!,
+				addWatermark: false,
 			});
 
 			await updateDesignRecord(payload.designId, thumbnailStorageKey.folder);
