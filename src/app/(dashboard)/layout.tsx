4445f1fe5ba2614bb76a49f98f4ae4079bb9caaa
priteshkadiwala <pk.45@hotmail.com>
2024-10-04 12:14:32 +0530

    Removed search bar from dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -89,7 +89,7 @@ export default async function Layout({
 					</div>
 				</div>
 				<div className="flex flex-col">
-					<header className="flex h-14 items-center gap-4 border-b bg-background px-4 lg:h-[60px] lg:px-6">
+					<header className="flex h-14 items-center gap-4 border-b bg-background px-4 lg:h-[60px] lg:px-6 lg:justify-end">
 						<Sheet>
 							<SheetTrigger asChild>
 								<Button
@@ -127,18 +127,6 @@ export default async function Layout({
 								</nav>
 							</SheetContent>
 						</Sheet>
-						<div className="w-full flex-1">
-							<form>
-								<div className="relative">
-									<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-									<Input
-										type="search"
-										placeholder="Search products..."
-										className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-									/>
-								</div>
-							</form>
-						</div>
 						<UserMenu />
 					</header>
 					{children}
