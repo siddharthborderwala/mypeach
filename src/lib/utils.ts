1265633081b7f063c62e30e546bd03c610cd2f31
Siddharth Borderwala <siddharthborderwala@gmail.com>
2024-10-15 23:30:10 +0530

    fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,11 +7,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const appBaseURL = (() => {
-	if (process.env.VERCEL_GIT_COMMIT_REF === "main") {
-		return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+	if (process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF === "main") {
+		return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
 	}
-	if (process.env.VERCEL_URL) {
-		return `https://${process.env.VERCEL_URL}`;
+	if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+		return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 	}
 	return "http://localhost:3000";
 })();
