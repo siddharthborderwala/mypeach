3fdbb11ddc443df6cdaa0f461041f2ebf56e4672
Siddharth Borderwala <siddharthborderwala@gmail.com>
2024-10-28 15:27:41 +0530

    fix domain

--- src/trigger/emails/util.ts
+++ src/trigger/emails/util.ts
@@ -1,6 +1,6 @@
 export const appBaseURL = (() => {
 	if (process.env.NODE_ENV === "production") {
-		return "https://mypeach.vercel.app";
+		return "https://mypeach.in";
 	}
 	return "http://localhost:3000";
 })();
