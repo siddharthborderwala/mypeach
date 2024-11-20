3bb4cf2366180189dff41857146c3233df701bad
priteshkadiwala <pk.45@hotmail.com>
2024-11-20 14:10:47 +0530

    Update generate-thumbnail.ts

--- src/trigger/generate-thumbnail.ts
+++ src/trigger/generate-thumbnail.ts
@@ -218,7 +218,7 @@ async function updateDesignRecord(
 export const generateThumbnailTask = task({
 	id: "generate-thumbnail",
 	machine: {
-		preset: "medium-1x",
+		preset: "medium-2x",
 	},
 	queue: {
 		name: "generate-thumbnail-queue",
