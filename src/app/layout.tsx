040300c3c2481983759ecc7aede03406d3682b72
Siddharth Borderwala <siddharthborderwala@gmail.com>
2024-11-24 15:35:12 +0530

    add social previews

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,39 @@ import { Manrope } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import { FlashToast } from "@/components/flash-toast";
 
-import "./globals.css";
+import { appBaseURL } from "@/lib/utils";
 import { GlobalQueryClient } from "./global-query-client";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { validateRequest } from "@/lib/auth/lucia";
 import { AuthProvider } from "@/contexts/auth";
+
+import "./globals.css";
 
 const manrope = Manrope({
 	subsets: ["latin"],
 	variable: "--font-manrope",
 });
 
+const title = "Peach";
+const description =
+	"India's marketplace for the latest TIFF textile design files ✨";
+
 export const metadata: Metadata = {
-	title: "Peach",
-	description: "Get the latest TIFF layered textile design files from Peach",
+	title,
+	description,
+	openGraph: {
+		title,
+		description,
+		images: [`${appBaseURL}/social.png`],
+		url: appBaseURL,
+	},
+	twitter: {
+		card: "summary_large_image",
+		title,
+		description,
+		images: [`${appBaseURL}/social.png`],
+		site: appBaseURL,
+	},
 };
 
 export default async function RootLayout({
